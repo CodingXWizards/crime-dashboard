3a659aeda68cd65c3b7b79fcced40ad2fc1c1d01
Suraj Patel <surajpatelsuraj55@gmail.com>
2024-12-22 17:00:29 +0530

    added page routing

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,10 +1,16 @@
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import InputSection from "./pages/InputSection";
 
 const App = () => {
   return (
-    <div>
-      <InputSection />
-    </div>
+    <Router>
+      <div>
+        <Routes>
+          <Route path="/entry" element={<InputSection />} />
+          <Route path="/" element={<div>Home Page</div>} />
+        </Routes>
+      </div>
+    </Router>
   );
 };
 
