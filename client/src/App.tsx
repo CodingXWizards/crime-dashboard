74e6c7f2313e842f7dc0630bd923543789717d7f
Suraj Patel <surajpatelsuraj55@gmail.com>
2024-12-22 22:15:37 +0530

    add: route for dashboard

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,6 +1,7 @@
 import { Routes, Route } from "react-router-dom";
 import InputSection from "@/pages/InputSection";
 import Home from "@/pages/home";
+import Header from "@/pages/dashboard/Header";
 
 const App = () => {
   return (
@@ -8,6 +9,7 @@ const App = () => {
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/entry" element={<InputSection />} />
+        <Route path="/dashboard" element={<Header />} />
       </Routes>
     </div>
   );
